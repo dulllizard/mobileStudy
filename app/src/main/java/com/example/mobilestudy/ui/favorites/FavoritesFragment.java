892c6097f44a9d67b974de6efa78e1e7ab0b71bb
Яшка <holyyakoff@gmail.com>
2024-04-02 11:02:29 +0700

    docs: добавлен install.md

--- app/src/main/java/com/example/mobilestudy/ui/favorites/FavoritesFragment.java
+++ app/src/main/java/com/example/mobilestudy/ui/favorites/FavoritesFragment.java
@@ -9,9 +9,6 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-
-import com.example.mobilestudy.R;
 import com.example.mobilestudy.databinding.FragmentFavoritesBinding;
 
 
--- app/src/main/java/com/example/mobilestudy/ui/map/MapFragment.java
+++ app/src/main/java/com/example/mobilestudy/ui/map/MapFragment.java
@@ -9,11 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-
-import com.example.mobilestudy.R;
 import com.example.mobilestudy.databinding.FragmentMapBinding;
-import com.example.mobilestudy.ui.home.HomeFragment;
+
 
 
 /**
