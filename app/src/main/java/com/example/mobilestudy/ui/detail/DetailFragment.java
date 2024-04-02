65ae88a16d3ac3cb5c7b02c15db8e400845970a6
Яшка <holyyakoff@gmail.com>
2024-04-02 08:30:55 +0700

    feat: добавлен DetailFragment

--- app/src/main/java/com/example/mobilestudy/ui/detail/DetailFragment.java
+++ app/src/main/java/com/example/mobilestudy/ui/detail/DetailFragment.java
@@ -1,4 +1,4 @@
-package com.example.mobilestudy.ui.favorites;
+package com.example.mobilestudy.ui.detail;
 
 import android.os.Bundle;
 
@@ -11,12 +11,12 @@ import android.view.ViewGroup;
 import com.example.mobilestudy.R;
 
 
-public class FavoritesFragment extends Fragment {
+public class DetailFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorites, container, false);
+        return inflater.inflate(R.layout.fragment_detail, container, false);
     }
 }
